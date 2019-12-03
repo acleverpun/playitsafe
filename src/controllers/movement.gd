7808b220ed1f01563f623e868187d5470821f4a0
Ross Hadden <rosshadden@gmail.com>
2019-12-03 01:44:29 -0500

    Increased player size.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/controllers/movement.gd
+++ src/controllers/movement.gd
@@ -6,7 +6,7 @@ const DOWN = Vector2(0, 1)
 const LEFT = Vector2(-1, 0)
 const RIGHT = Vector2(1, 0)
 
-export var speed = 50.0
+export var speed = 200.0
 export var runMod = 4.0
 
 onready var host = get_parent().get_parent()
