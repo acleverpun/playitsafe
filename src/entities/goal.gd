93b1ef9034a888c4ee3a27419148832783989053
Ross Hadden <rosshadden@gmail.com>
2019-12-04 00:09:32 -0500

    Made goal load scene in the zones viewport.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/entities/goal.gd
+++ src/entities/goal.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+onready var Zone = load("res://src/zones/lobby.tscn")
+onready var zones = $"/root/main/zones"
+
 func _ready():
 	connect("body_entered", self, "onEnter")
 
 func onEnter(body):
-	get_tree().change_scene("res://src/zones/lobby.tscn")
+	zones.get_child(0).queue_free()
+	zones.add_child(Zone.instance())
